d2485c8b7a95fdcb3661e32c6416a4cc8f6ac6b8
Luka Panio <lukapanio@gmail.com>
2025-11-19 17:12:51 +0100

    finish-ohd: Get rid of root

--- finish-ohd.sh
+++ finish-ohd.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+export PATH=$PATH:/usr/sbin/
 rm ./output/image/ohd_config.img
 truncate --size 256M ./output/image/ohd_config.img
 mkfs.vfat -F 32 ./output/image/ohd_config.img
 mkdir output/mnt
-sudo mount ./output/image/ohd_config.img output/mnt
-sudo touch output/mnt/config.txt
-sudo mkdir output/mnt/openhd
-sudo touch output/mnt/openhd/rv1106.txt
-sudo touch output/mnt/openhd/air.txt
-sudo umount output/mnt
+export MTOOLS_SKIP_CHECK=1
+mmd   -i ./output/image/ohd_config.img ::/openhd
+echo -n "" | mcopy -i ./output/image/ohd_config.img - ::/config.txt
+echo -n "" | mcopy -i ./output/image/ohd_config.img - ::/openhd/rv1106.txt
+echo -n "" | mcopy -i ./output/image/ohd_config.img - ::/openhd/air.txt
 resize2fs ./output/image/rootfs.img 1G
 genimage --config genimage.cfg --inputpath ./output/image --outputpath ./output/genimage --rootpath ./rootfs-empty
