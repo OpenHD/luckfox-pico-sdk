f4f13a39cb94210d4ddc97e908272417fb672407
OpenHD Raphael <raphael@openhdfpv.org>
2026-04-11 18:15:39 +0200

    Buildroot: install OpenHD from subdir output

--- sysdrv/tools/board/buildroot/package/OpenHD/openhd.mk
+++ sysdrv/tools/board/buildroot/package/OpenHD/openhd.mk
@@ -117,7 +117,7 @@ OPENHD_PRE_CONFIGURE_HOOKS += OPENHD_BUILD_ARTOSYN_SDK
 
 define OPENHD_INSTALL_TARGET_CMDS
 	$(info OpenHD Build Directory: $(@D))
-	$(INSTALL) -D -m 0755 $(@D)/openhd $(TARGET_DIR)/usr/bin/openhd
+	$(INSTALL) -D -m 0755 $(@D)/$(OPENHD_SUBDIR)/openhd $(TARGET_DIR)/usr/bin/openhd
 	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_YOURTREE_PATH)/package/OpenHD/start.sh \
 		$(TARGET_DIR)/etc/init.d/S99openhd
 endef
